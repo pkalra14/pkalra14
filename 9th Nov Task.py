da30d98229f8ef65fc30dcde1b96980da38a4c7d
Prachi Kalra <pkalra14@gmail.com>
2024-11-17 20:05:45 +0530

    9th Nov Task

---
+++ 9th Nov Task.py
@@ -0,0 +1,20 @@
+# Task 1 for the Today
+# Take a 2 input from the user
+# Print the Quotient and Remainder
+# 15 ->  num1
+# 2 -> num2
+# Q -> 7
+# R -> 1
+
+num1 = 15
+num2 = 2
+
+div = 15 / 2
+
+print("div is : ", div)
+
+
+# Task 2 for the Today
+# Take a 3 input from the user
+# perform the sub, sub, mul and div
+
